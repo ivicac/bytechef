275d377f6c1b3ba4dc19b44daa0b1eb577ebe988
Ivica Cardic <ivica.cardic@gmail.com>
2026-06-04 12:26:16 +0200

    520 SF

--- server/ee/libs/embedded/embedded-configuration/embedded-configuration-service/src/main/java/com/bytechef/ee/embedded/configuration/facade/AutomationWorkflowProjectFacadeImpl.java
+++ server/ee/libs/embedded/embedded-configuration/embedded-configuration-service/src/main/java/com/bytechef/ee/embedded/configuration/facade/AutomationWorkflowProjectFacadeImpl.java
@@ -232,8 +232,7 @@ public class AutomationWorkflowProjectFacadeImpl implements AutomationWorkflowPr
 
     @Override
     public List<AutomationWorkflowProjectDTO> getProjects() {
-        return projectService
-            .getProjects()
+        return projectService.getProjects()
             .stream()
             .filter(project -> project.getName() != null && project.getName()
                 .startsWith(MARKER)
@@ -244,8 +243,7 @@ public class AutomationWorkflowProjectFacadeImpl implements AutomationWorkflowPr
 
     @Override
     public List<AutomationWorkflowProjectDTO> getPublishedProjects() {
-        return projectService
-            .getProjects()
+        return projectService.getProjects()
             .stream()
             .filter(project -> project.getName() != null && Strings.CS.startsWith(project.getName(), MARKER) &&
                 Objects.equals(project.getWorkspaceId(), Workspace.DEFAULT_WORKSPACE_ID))
