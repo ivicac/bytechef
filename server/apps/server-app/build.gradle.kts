3e2825a05b923ae5f5f21907938b0675bc1be229
Ivica Cardic <ivica.cardic@gmail.com>
2026-06-07 09:20:33 +0200

    - copilot - Add Property Copilot API types
    property copilot

--- server/apps/server-app/build.gradle.kts
+++ server/apps/server-app/build.gradle.kts
@@ -264,6 +264,7 @@ dependencies {
     implementation(project(":server:libs:modules:task-dispatchers:suspend"))
     implementation(project(":server:libs:modules:task-dispatchers:terminate"))
 
+    implementation(project(":server:ee:libs:ai:ai-copilot:ai-copilot-graphql"))
     implementation(project(":server:ee:libs:ai:ai-copilot:ai-copilot-rest"))
     implementation(project(":server:ee:libs:ai:ai-copilot:ai-copilot-service"))
     implementation(project(":server:libs:ai:ai-copilot:ai-copilot-service"))
