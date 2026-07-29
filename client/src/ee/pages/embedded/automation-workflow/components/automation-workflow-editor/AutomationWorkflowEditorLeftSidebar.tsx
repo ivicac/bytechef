c0cf9ee6211a61174fa716fdf87d9fb46d4c75b5
Ivica Cardic <ivica.cardic@gmail.com>
2026-07-29 13:06:36 +0200

    --- client - Rename sidebar "Workflow" create button to "New Workflow"

--- client/src/ee/pages/embedded/automation-workflow/components/automation-workflow-editor/AutomationWorkflowEditorLeftSidebar.tsx
+++ client/src/ee/pages/embedded/automation-workflow/components/automation-workflow-editor/AutomationWorkflowEditorLeftSidebar.tsx
@@ -188,7 +188,7 @@ const AutomationWorkflowEditorLeftSidebar = ({currentWorkflowId}: AutomationWork
                 <Button
                     className="w-full [&_svg]:size-5"
                     icon={<PlusIcon />}
-                    label="Workflow"
+                    label="New Workflow"
                     onClick={() => setShowWorkflowDialog(true)}
                     variant="secondary"
                 />
--- client/src/ee/pages/embedded/integration/components/integrations-sidebar/IntegrationsLeftSidebar.tsx
+++ client/src/ee/pages/embedded/integration/components/integrations-sidebar/IntegrationsLeftSidebar.tsx
@@ -165,7 +165,7 @@ const IntegrationsLeftSidebar = ({
                 <Button
                     className="w-full [&_svg]:size-5"
                     icon={<PlusIcon />}
-                    label="Workflow"
+                    label="New Workflow"
                     onClick={() => setShowWorkflowDialog(true)}
                     variant="secondary"
                 />
